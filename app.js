2b54772dc034c0356001b5dbf133a7e6a988d337
Robin <robin.kriisa@voco.ee>
2022-11-15 13:11:08 +0200

    Add book function

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+// UI object
+const ui = new UI()
 
+// elements
+const form = document.querySelector('#add-book');
+
+// events
+form.addEventListener('submit', addBook);
+
+
+function addBook(event) {
+    // get form input value
+
+    const titleInput = document.querySelector(`#title`)
+    const authorInput = document.querySelector(`#author`)
+    const isbnInput = document.querySelector(`#isbn`)
+
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let isbn = isbnInput.value;
+
+    // create book
+    const book = new Book(title, author, isbn)
+    // add book value to visual by UI object
+    ui.addBook(book)
+
+
+    titleInput.value = "";
+    authorInput.value = "";
+    isbnInput.value = "";
+    event.preventDefault();
+}
+
+
+    //add task value to localStorage
+    //addBookToLS(book)
